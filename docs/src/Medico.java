5832a155c993557542159916916a10b1a4fc46c2
alanmilagre <alanmilagre457@gmail.com>
2025-11-19 20:26:29 -0300

    Medico.java consertado

--- docs/src/Medico.java
+++ docs/src/Medico.java
@@ -4,13 +4,13 @@ public class Medico extends Usuario {
     private String especialidade;
     private String sexo;
 
-// public Medico(int id, int crm, String especialidade, String sexo){
-//     this.id = id;
-//     this.crm = crm;
-//     this.especialidade = especialidade;
-//     this.sexo = sexo;
-// }
-
+ public Medico(int id, int crm, String especialidade, String sexo){
+    super("", "Médico", "", "", "");   
+    this.id = id;
+    this.crm = crm;
+    this.especialidade = especialidade;
+    this.sexo = sexo;
+}
 
 
 @Override // pra garantir q estamos sobrescrevendo o toString() da classe Object e para exibir informações de usuario
