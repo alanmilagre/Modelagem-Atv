aded2f37a68712afa3059d2f089fb72b465d3741
alanmilagre <alanmilagre457@gmail.com>
2025-11-19 20:19:55 -0300

    Consertei a syntax no medico mas ainda tá com um erro

---
+++ docs/src/Medico.java
@@ -0,0 +1,28 @@
+public class Medico extends Usuario {
+    public int id;
+    private int crm;
+    private String especialidade;
+    private String sexo;
+
+// public Medico(int id, int crm, String especialidade, String sexo){
+//     this.id = id;
+//     this.crm = crm;
+//     this.especialidade = especialidade;
+//     this.sexo = sexo;
+// }
+
+
+
+@Override // pra garantir q estamos sobrescrevendo o toString() da classe Object e para exibir informações de usuario
+   public String toString() {
+    return "Id: " + id +
+           ", CRM: " + crm +
+           ", Especialidade: " + especialidade +
+           ", Sexo: " + sexo; 
+}
+
+public static void visualizar_agenda() {
+// logica do método
+
+    }
+}
--- docs/src/main.java
+++ docs/src/main.java
@@ -32,5 +32,6 @@ public class Main {
         } while (opcao != 4);
         
         sc.close();
+
     }
 }
