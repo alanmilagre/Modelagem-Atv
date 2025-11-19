19fc353e1ba4bfcfce5bf3c335fd1b2a80cb3f23
alanmilagre <alanmilagre457@gmail.com>
2025-11-19 20:05:21 -0300

    Mudanças básicas na main.java

--- docs/src/main.java
+++ docs/src/main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int opcao = 0;
         do{
             System.out.println("\n╔════════════════════════════════════════════════════╗");
             System.out.println("║                  Clinica.IO                   ║");
             System.out.println("╚════════════════════════════════════════════════════╝");
             System.out.print(" 1 - Fazer login\n 2 - Fazer cadastro\n 3 - Gerar usuários(Teste)- \n 4 - Sair\nDigite a opção desejada: ");
-            Local opcao = sc.nextInt();
+             opcao = sc.nextInt();
             sc.nextLine();    
 
             switch (opcao) {
@@ -23,10 +23,10 @@ public class Main {
                     // Chamar método de gerar usuários de teste
                     break;
                 case 4:
-                    System.out.println("Saindo do sistema...");
+                    System.out.println('\n' + "Saindo do sistema...");
                     break;
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println('\n' + "Opção inválida. Tente novamente.");
             }        
     
         } while (opcao != 4);
